ba773dfabc10fe5594a01d8aefa9681ae009ff2c
mstr0m <61348380+mstr0m@users.noreply.github.com>
2022-03-13 19:35:06 +0300

    Create install-kubernetes.sh

---
+++ install-kubernetes.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+modprobe overlay
+modprobe br_netfilter
+
+
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+apt-get update
+apt-get install -y kubelet=1.20.1-00 kubeadm=1.20.1-00 kubectl=1.20.1-00
