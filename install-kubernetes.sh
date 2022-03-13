b22977effcafdab26624a77684f6b5f876fca375
mstr0m <61348380+mstr0m@users.noreply.github.com>
2022-03-13 19:37:25 +0300

    Update install-kubernetes.sh

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -7,3 +7,6 @@ echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 apt-get update
 apt-get install -y kubelet=1.20.1-00 kubeadm=1.20.1-00 kubectl=1.20.1-00
+
+#On Master node
+#kubeadm init --pod-network-cidr=10.244.0.0/16
